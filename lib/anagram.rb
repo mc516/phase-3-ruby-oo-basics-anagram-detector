912b2e36ecdbc679389ccba30424f20acfc77777
Mickey <mickeyc11111@gmail.com>
2022-09-07 18:31:36 -0400

    commit tree 9ef921182f6b23465f725c16fa89c557d7d0507e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+    def initialize(str)
+        @str = str
+    end
+
+    def match(words)
+        match = []
+        if words != @str
+            match
+        elsif words.chars.sort == @str.chars.sort
+           match << %w[words]
+        end
+    end
+end
